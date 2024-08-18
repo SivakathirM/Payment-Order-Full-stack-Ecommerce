4b50521b00ff2acdb56442027bbb6abe2667a7cd
SivakathirM <sivakathir215@gmail.com>
2024-08-18 11:35:02 +0530

    Update paymentController.js

--- backend/controller/order/paymentController.js
+++ backend/controller/order/paymentController.js
@@ -41,7 +41,7 @@ const paymentController=async(request,response)=>{
                     quantity:item.quantity
                 }
             }),
-            success_url:`${process.env.FRONTEND_URL}/order`,
+            success_url:`${process.env.FRONTEND_URL}`,
             cancel_url:`${process.env.FRONTEND_URL}/cencel`,
         }
 
